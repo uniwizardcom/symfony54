2d8e0a450b6f35002556359a3e8b2096d41852bc
Wojciech Niewiadomski gmail <niewiadomski.wojciech@gmail.com>
2024-12-05 14:02:11 +0100

    Added IndexController.php

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'homepage')]
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
